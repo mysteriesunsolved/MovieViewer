2dd8c248b475e71f0c25a1b355f12895f12f5c26
Sanaya Old <Sanaya@sanayas-mac-mini.local>
2016-01-11 23:31:16 -0800

    Added a collectionView, ability to switch between collection and table views, added a navigation bar, customized UI

---
+++ MovieViewer/MovieCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionCell.swift
+//  MovieViewer
+//
+//  Created by Sanaya Sanghvi on 1/10/16.
+//  Copyright © 2016 Sanaya Sanghvi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var postercollectionView: UIImageView!
+    
+    @IBOutlet weak var titlecollectionLabel: UILabel!
+    
+}
--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -8,30 +8,52 @@
 
 import UIKit
 import AFNetworking
-import EZLoadingActivity
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
   
     var refreshControl: UIRefreshControl!
   
     @IBOutlet weak var tableView: UITableView!
-    
-    @IBOutlet var tapGesture: UITapGestureRecognizer!
   
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+   
+    @IBOutlet weak var barButton: UIBarButtonItem!
+    
+    
+    @IBOutlet weak var switchView: UISwitch!
+  
+ 
+    
+    var hidden = Bool()
+    
+    
     var movies: [NSDictionary]?
     
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-      
-      
+        
         tableView.dataSource = self
         tableView.delegate = self
-      
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        navigationController!.navigationBar.barTintColor = UIColor.blackColor()
+        navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.orangeColor()]
+        
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
         self.refreshControl.tintColor = UIColor .blueColor()
         tableView.addSubview(refreshControl)
+        collectionView.addSubview(refreshControl)
+        
+        errorLabel.hidden = true
+        
+        tableView.hidden = true
+        
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -52,6 +74,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             
                             self.movies = responseDictionary["results"] as! [NSDictionary]
                             self.tableView.reloadData()
+                            self.collectionView.reloadData()
+                            
                             
                     }
                 }
@@ -66,21 +90,25 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
+    //Table View 
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       
       if let movies = movies {
         return movies.count
       } else {
         
-        EZLoadingActivity.hide(success: false, animated: true)
+        errorLabel.hidden = false
         return 0
       }
       
     }
     
+    //TableView Data
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-      
+      let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+        
       let movie = movies![indexPath.row]
       let title = movie["title"] as! String
       let overview = movie["overview"] as! String
@@ -93,12 +121,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
       cell.titleLabel.text = title
       cell.overviewLabel.text = overview
       cell.posterView.setImageWithURL(imageURL!)
-      
-      
+        
       print("row \(indexPath.row)")
+        
       return cell
         
     }
+    
+    //Refresh Commands
     
     func delay(delay:Double, closure:()->()) {
         dispatch_after(
@@ -106,6 +136,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 DISPATCH_TIME_NOW,
                 Int64(delay * Double(NSEC_PER_SEC))
             ),
+            
             dispatch_get_main_queue(), closure)
     }
     
@@ -116,16 +147,65 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
    
-    
-  
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Collection View
+   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if let movies = movies {
+            return movies.count
+        } else {
+            
+            errorLabel.hidden = false
+            return 0
+        }
+        
     }
-    */
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MovieCollectionCell", forIndexPath: indexPath) as! MovieCollectionCell
+        
+        
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let posterPath = movie["poster_path"] as! String
+        
+        let baseURL = "http://image.tmdb.org/t/p/w500/"
+        
+        let imageURL = NSURL(string: baseURL + posterPath)
+        
+        cell.titlecollectionLabel.text = title
+        cell.postercollectionView.setImageWithURL(imageURL!)
+        
+        
+        print("row \(indexPath.row)")
+        
+        return cell
+        
+        
+        }
+    
+    //Button Switch
+    
+    @IBAction func viewSwitch(sender: UISwitch) {
+        
+        if switchView.on {
+            tableView.hidden = true
+            collectionView.hidden = false
+            collectionView.addSubview(refreshControl)
+        } else {
+            tableView.hidden = false
+            collectionView.hidden = true
+            tableView.addSubview(refreshControl)
+        }
+        
+
+    }
+   
+    
+    
+   
+    
 
 }
+    
+
